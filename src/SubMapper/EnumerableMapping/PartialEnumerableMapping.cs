298133cf5a399a1a3dd205ddd3aa5362c7ea78c5
Joona Luoma <joona.luoma@student.tut.fi>
2017-01-12 18:58:48 +0200

    Remove comments

--- src/SubMapper/EnumerableMapping/PartialEnumerableMapping.cs
+++ src/SubMapper/EnumerableMapping/PartialEnumerableMapping.cs
@@ -21,13 +21,9 @@ namespace SubMapper.EnumerableMapping
         {
             var subIEnumInfo = getSubIExpr.GetMapPropertyInfo();
             var subJInfo = getSubJExpr.GetMapPropertyInfo();
-
-            // TODO: refactor
-            // TODO: does rotated map properly? SubIEnum -> SubA??? Nope, they dont
-            var subIEnumPropertyInfo = subIEnumInfo.PropertyInfo; //?? (IsRotated ? prevSubMap.SubBPropertyInfo : prevSubMap.SubAPropertyInfo); this should be useless -> always non null
+            
+            var subIEnumPropertyInfo = subIEnumInfo.PropertyInfo;
             var subJPropertyInfo = subJInfo.PropertyInfo ?? prevSubMap.HalfSubMapPair.JHalfSubMap.PropertyInfo;
-
-            // TODO: Rotate prevSubMap if it should be
 
             var result = new SubMap
             {
@@ -85,29 +81,6 @@ namespace SubMapper.EnumerableMapping
                     }
                 },
 
-                //GetSubAFromA = na =>
-                //{
-                //    var subIEnum = subIEnumInfo.Getter(na);
-                //    if (subIEnum == null) return null;
-                //    // TODO: make WhereMatchesContainer consume objets instead of derived types
-                //    var subIItem = _whereMatchess.First().GetFirstSubAItemFromSubAEnumWhereMatches((TSubIEnum)subIEnum);
-                //    if (subIItem == null) return null;
-                //    return prevSubMap.GetSubAFromA(subIItem);
-                //},
-                //GetSubBFromB = nb =>
-                //{
-                //    var subJ = subJInfo.Getter(nb);
-                //    if (subJ == null) return null;
-                //    return prevSubMap.GetSubBFromB(subJ);
-                //},
-
-                //// TODO
-                //SubAPropertyName = "TODO", //subAEnumInfo.Setter == null ? prevSubMap.SubAPropertyName : (aInfo.PropertyName + "." + prevSubMap.SubAPropertyName),
-                //SubBPropertyName = subJInfo.Setter == null ? prevSubMap.SubBPropertyName : (subJInfo.PropertyName + "." + prevSubMap.SubBPropertyName),
-
-                //SubAPropertyInfo = subIEnumPropertyInfo,
-                //SubBPropertyInfo = subJPropertyInfo,
-
                 MetaMap = new Lazy<MetaMap>(() => new MetaMap
                 {
                     MetadataType = typeof(PartialEnumerableMappingMetadata),
@@ -127,70 +100,6 @@ namespace SubMapper.EnumerableMapping
 
                     SubMetaMap = prevSubMap.MetaMap.Value
                 })
-
-                //MetaMap =
-                //IsRotated ?
-                //new Lazy<MetaMap>(() => new MetaMap
-                //{
-                //    MetadataType = typeof(PartialEnumerableMappingMetadata),
-                //    Metadata = new PartialEnumerableMappingMetadata
-                //    {
-                //        // TODO: WHERE infos
-
-                //        IEnumPropertyInfo = subIEnumPropertyInfo,
-                //        SubIEnumPropertyInfo = prevSubMap.SubBPropertyInfo,
-
-                //        JPropertyInfo = subJPropertyInfo,
-                //        SubJPropertyInfo = prevSubMap.SubAPropertyInfo,
-                //        IsJAndSubJDifferent = subJPropertyInfo.Name != prevSubMap.SubAPropertyInfo.Name, // TODO
-
-                //        IsRotated = IsRotated
-                //    },
-                //    SubMetaMap = prevSubMap.MetaMap.Value
-                //}) :
-                //new Lazy<MetaMap>(() => new MetaMap
-                //{
-                //    MetadataType = typeof(PartialEnumerableMappingMetadata),
-                //    Metadata = new PartialEnumerableMappingMetadata
-                //    {
-                //        // TODO: WHERE infos
-
-                //        IEnumPropertyInfo = subIEnumPropertyInfo,
-                //        SubIEnumPropertyInfo = prevSubMap.SubAPropertyInfo,
-
-                //        JPropertyInfo = subJPropertyInfo,
-                //        SubJPropertyInfo = prevSubMap.SubBPropertyInfo,
-                //        IsJAndSubJDifferent = subJPropertyInfo.Name != prevSubMap.SubBPropertyInfo.Name, // TODO
-
-                //        IsRotated = IsRotated
-                //    },
-                //    SubMetaMap = prevSubMap.MetaMap.Value
-                //}),
-
-                //SetSubAFromA = (na, v) =>
-                //{
-                //    if (v == null) return;
-                //    var subIEnum = subIEnumInfo.Getter(na);
-
-                //    // TODO: allow multiple
-                //    var whereMatches = _whereMatchess.First();
-                //    var subIItem = subIEnum != null ? (object)whereMatches.GetFirstSubAItemFromSubAEnumWhereMatches((TSubIEnum)subIEnum) : null;
-                //    if (subIItem == null)
-                //    {
-                //        subIItem = new TSubIItem();
-                //        typeof(TSubIItem).GetMapPropertyInfo(whereMatches.ValuePropertyName).Setter(subIItem, whereMatches.ValuePropertyValue);
-                //        subIEnum = _getSubIEnumWithAddedSubIItem((TSubIEnum)subIEnum, (TSubIItem)subIItem);
-                //        subIEnumInfo.Setter(na, subIEnum);
-                //    }
-
-                //    prevSubMap.SetSubAFromA(subIItem, v);
-                //},
-                //SetSubBFromB = (nb, v) =>
-                //{
-                //    if (v == null) return;
-                //    if (subJInfo.Getter(nb) == null) subJInfo.Setter(nb, new TSubJ());
-                //    prevSubMap.SetSubBFromB(subJInfo.Getter(nb), v);
-                //}
             };
 
             return result;
