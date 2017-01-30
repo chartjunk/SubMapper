e711fb64c70c5e22c874343cdccb45fd5a1f2861
Joona Luoma <joona.luoma@student.tut.fi>
2017-01-30 19:41:53 +0200

    Clean up Handle residuals

--- src/SubMapper/Mapping.cs
+++ src/SubMapper/Mapping.cs
@@ -1,35 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using SubMapper.SubMapping;
-using SubMapper.EnumerableMapping;
-using System.Collections;
-
-namespace SubMapper
+﻿namespace SubMapper
 {
     public static partial class Mapping
     {
         public static BaseMapping<TA, TB> FromTo<TA, TB>()
             => new BaseMapping<TA, TB>();
-
-        public static SubMapping<TA, TB, TSubA, TSubB>
-            Using<TA, TB, TSubA, TSubB>(
-            SubMappingHandle<TA, TB, TSubA, TSubB> subMappingHandle)
-            where TSubA : new()
-            where TSubB : new()
-            => new SubMapping<TA, TB, TSubA, TSubB>();
-
-        public static FromEnumerableMapping<TA, TB, IEnumerable<TSubAItem>, TSubB, TSubAItem> 
-            Using<TA, TB, TSubB, TSubAItem>(
-            FromEnumerableMappingHandle<TA, TB, IEnumerable<TSubAItem>, TSubB, TSubAItem> fromEnumerableMappingHandle)
-            where TSubAItem : new()
-            where TSubB : new()
-            => new FromEnumerableMapping<TA, TB, IEnumerable<TSubAItem>, TSubB, TSubAItem>();
-
-        public static ToEnumerableMapping<TA, TB, TSubA, IEnumerable<TSubBItem>, TSubBItem>
-            Using<TA, TB, TSubA, TSubBItem>(
-            ToEnumerableMappingHandle<TA, TB, TSubA, IEnumerable<TSubBItem>, TSubBItem> toEnumerableMappingHandle)
-            where TSubA : new()
-            where TSubBItem : new()
-            => new ToEnumerableMapping<TA, TB, TSubA, IEnumerable<TSubBItem>, TSubBItem>();
     }
 }
--- src/SubMapper/SubMapping/BaseMappingExtensions.cs
+++ src/SubMapper/SubMapping/BaseMappingExtensions.cs
@@ -10,15 +10,12 @@ namespace SubMapper
             this BaseMapping<TA, TB> source,
             Expression<Func<TA, TSubA>> getSubAExpr,
             Expression<Func<TB, TSubB>> getSubBExpr,
-            //Func<SubMappingHandle<TA, TB, TSubA, TSubB>, BaseMapping<TSubA, TSubB>> getInnerBaseMapping)
-            Func<SubMapping<TA, TB, TSubA, TSubB>, BaseMapping<TSubA, TSubB>> getMapping)
+            Func<SubMapping<TA, TB, TSubA, TSubB>, BaseMapping<TSubA, TSubB>> getInnerBaseMapping)
             where TSubA : new()
             where TSubB : new()
         {
             var subMapping = new SubMapping<TA, TB, TSubA, TSubB>();
-            var innerBaseMapping = getMapping(subMapping); 
-            //var innerBaseMapping = getInnerBaseMapping(new SubMappingHandle<TA, TB, TSubA, TSubB>());
-            //var subMapping = innerBaseMapping.Extensibility.DerivedMapping as SubMapping<TA, TB, TSubA, TSubB>;
+            var innerBaseMapping = getInnerBaseMapping(subMapping);            
             var fullSubMaps = subMapping.GetSubMapsWithAddedPath(getSubAExpr, getSubBExpr);
             source.Extensibility.SubMaps.AddRange(fullSubMaps);
             return source;
