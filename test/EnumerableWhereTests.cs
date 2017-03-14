2e0f99f70be9404d9c5685e0e1e793536d3dec68
Joona Luoma <joona.luoma@student.tut.fi>
2017-03-14 15:19:39 +0200

    Test where on enumerables mapping

--- test/EnumerableWhereTests.cs
+++ test/EnumerableWhereTests.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SubMapper.EnumerableMapping.Adders;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace SubMapper.UnitTest
@@ -8,7 +9,7 @@ namespace SubMapper.UnitTest
     public class EnumerableWhereTests
     {
         [TestMethod]
-        public void MapUsingMultipleWhere()
+        public void MapPartialUsingMultipleWhere()
         {
             var mapping = Mapping.FromTo<SourceType, TargetType>()
                 .FromEnum(s => s.Enumerable1, t => t, fromEnumMapping => fromEnumMapping
@@ -26,6 +27,23 @@ namespace SubMapper.UnitTest
 
             Assert.AreEqual(si.Enumerable1.First(i => i.EnumerableInt1 == 1 && i.EnumerableString1 == "Enumerable1 String1").EnumerableSub1.EnumerableSubString1, ti.String1);
             Assert.AreEqual(si2.Enumerable1.First(i => i.EnumerableInt1 == 1 && i.EnumerableString1 == "Enumerable1 String1").EnumerableSub1.EnumerableSubString1, ti.String1);
+        }
+
+        [TestMethod]
+        public void MapEnumerablesUsingWhere()
+        {
+            var mapping = Mapping.FromTo<SourceType, TargetType>()
+                .Enums(s => s.EnumerableList1, t => t.EnumerableList1, eM => eM
+                    .UsingArrayConcatAdder()
+                    .Where(s => s.EnumerableInt1 == 1, t => t.EnumerableInt1 == 999)
+                    .Map(s => s.EnumerableString1, t => t.EnumerableString1));
+
+            var si = SourceType.GetTestInstance();
+            var ti = new TargetType();
+            var si2 = new SourceType();
+
+            mapping.TranslateAToB(si, ti);
+            mapping.TranslateBToA(si2, ti);
         }
     }
 }
