f0f6b14d3f07e0142d7dddd7673abe538b7fc6ca
Joona Luoma <joona.luoma@student.tut.fi>
2017-03-14 11:07:40 +0200

    Test enumerables mapping

--- test/InitialFunctionalityTests.cs
+++ test/InitialFunctionalityTests.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Linq;
+using System.Linq.Expressions;
+using System;
+using System.Collections.Generic;
 
 namespace SubMapper.UnitTest
 {
@@ -86,6 +89,54 @@ namespace SubMapper.UnitTest
 
             Assert.AreEqual(si.String1, ti.Enumerable1.First(i => i.EnumerableInt1 == 1).EnumerableString1);
             Assert.AreEqual(si2.String1, ti.Enumerable1.First(i => i.EnumerableInt1 == 1).EnumerableString1);
+        }
+
+        [TestMethod]
+        public void MapEnumerables()
+        {
+            var mapping = Mapping.FromTo<SourceType, TargetType>()
+                .Enums(s => s.Enumerable1, t => t.Enumerable1, enumsMapping => enumsMapping
+                    .UsingAdder(
+                        (c, s) => (c ?? new SourceType.SourceEnumerableType[] { }).Concat(new[] { s }),
+                        (c, t) => (c ?? new TargetType.TargetEnumerableType[] { }).Concat(new[] { t }))
+                    .Map(s => s.EnumerableInt1, t => t.EnumerableInt1)
+                    .Map(s => s.EnumerableString1, t => t.EnumerableString1)
+                    .Sub(s => s.EnumerableSub1, t => t.EnumerableSub1, subMapping => subMapping
+                        .Map(s => s.EnumerableSubString1, t => t.EnumerableSubString1)));
+
+            var si = SourceType.GetTestInstance();
+            var ti = new TargetType();
+            var si2 = new SourceType();
+
+            mapping.TranslateAToB(si, ti);
+            mapping.TranslateBToA(si2, ti);
+
+            var siFirst = si.Enumerable1.ElementAt(0);
+            var siSecond = si.Enumerable1.ElementAt(1);
+            var tiFirst = ti.Enumerable1.ElementAt(0);
+            var tiSecond = ti.Enumerable1.ElementAt(1);
+            var si2First = si2.Enumerable1.ElementAt(0);
+            var si2Second = si2.Enumerable1.ElementAt(1);
+
+            var assertEnumerablesEqual = new Action<SourceType.SourceEnumerableType, TargetType.TargetEnumerableType>((s, t) =>
+            {
+                Assert.AreEqual(s.EnumerableInt1, t.EnumerableInt1);
+                Assert.AreEqual(s.EnumerableString1, t.EnumerableString1);
+                var sSub = s.EnumerableSub1;
+                var tSub = t.EnumerableSub1;
+                if(sSub != null || tSub != null)
+                    Assert.AreEqual(sSub.EnumerableSubString1, tSub.EnumerableSubString1);
+                // else both of them are null -> are equal
+            });
+
+            foreach(var p in new[]
+            {
+                Tuple.Create(siFirst, tiFirst),
+                Tuple.Create(siSecond, tiSecond),
+                Tuple.Create(si2First, tiFirst),
+                Tuple.Create(si2Second, tiSecond)
+            })
+                assertEnumerablesEqual(p.Item1, p.Item2);
         }
 
         [TestMethod]
