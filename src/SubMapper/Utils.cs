3e6f1216d82e2dd39902f0b01b8aa0da2c447902
Joona Luoma <joona.luoma@student.tut.fi>
2017-04-25 15:01:46 +0300

    Clean Utils

--- src/SubMapper/Utils.cs
+++ src/SubMapper/Utils.cs
@@ -18,40 +18,5 @@ namespace SubMapper
             // Is of type i => i
             else return null;
         }
-
-        public static MapPropertyInfo GetMapPropertyInfo<TI, TValue>(
-            this Expression<Func<TI, TValue>> getIExpr)
-            => new MapPropertyInfo(getIExpr.GetPropertyInfo());
-
-        public static MapPropertyInfo GetMapPropertyInfo(
-            this Type targetType, string propertyName)
-            => new MapPropertyInfo(targetType.GetProperty(propertyName));
-
-        public class MapPropertyInfo
-        {
-            public Func<object, object> Getter { get; private set; }
-            public Action<object, object> Setter { get; private set; }
-            public string PropertyName { get; private set; }
-            public PropertyInfo PropertyInfo { get; set; }
-
-            public MapPropertyInfo(PropertyInfo propertyInfo)
-            {
-                // TODO: refactor this logic, separated in two places
-                if (propertyInfo != null)
-                {
-                    Getter = i => propertyInfo.GetValue(i);
-                    Setter = (i, v) => propertyInfo.SetValue(i, v);
-                    PropertyName = propertyInfo.Name;
-                    PropertyInfo = propertyInfo;
-                }
-                else
-                {
-                    Getter = i => i;
-                    Setter = null;
-                    PropertyName = "TODO";
-                    PropertyInfo = null;
-                }
-            }
-        }
     }
 }
